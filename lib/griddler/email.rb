94161675037609827e542946f2a278bc984a9330
Brian Ko <brianswko@gmail.com>
2018-10-23 14:12:23 -0700

    Read raw_body in Email params

--- lib/griddler/email.rb
+++ lib/griddler/email.rb
@@ -32,7 +32,7 @@ module Griddler
       @body = extract_body
       @raw_text = params[:text]
       @raw_html = params[:html]
-      @raw_body = @raw_text.presence || @raw_html
+      @raw_body = params[:raw_body].presence || @raw_text.presence || @raw_html
 
       @headers = extract_headers
 
